0ca5000b7a0087890b5f8a1d028ba1d038300912
Xiomara <xiomy6789@gmail.com>
2026-02-08 18:51:56 -0500

    actualizacion

---
+++ Petcare/src/test/java/com/edu/sena/Petcare/PetcareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.sena.Petcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetcareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
